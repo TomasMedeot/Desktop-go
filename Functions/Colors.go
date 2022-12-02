45a7ee9ff7fd7f0a589c8f4a0578f3f4efeec92c
tomi medeot <tomimedeot@gmail.com>
2022-12-02 13:17:25 -0300

    colors finished

--- Functions/Colors.go
+++ Functions/Colors.go
@@ -10,16 +10,34 @@ import (
 )
 
 func Color(window fyne.Window) {
-	text := canvas.NewText("", color.Black)
+	color := color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+	text := canvas.NewText("", color)
 	inp := widget.NewEntry()
 	green := widget.NewCheck("Green", func(value bool) {
+		if value {
+			color.G = 255
+		} else {
+			color.G = 0
+		}
 	})
 	red := widget.NewCheck("Red", func(value bool) {
+		if value {
+			color.R = 255
+		} else {
+			color.R = 0
+		}
 	})
 	blue := widget.NewCheck("Blue", func(value bool) {
+		if value {
+			color.B = 255
+		} else {
+			color.B = 0
+		}
 	})
 	button := widget.NewButton("Colorear", func() {
 		text.Text = inp.Text
+		text.Color = color
+		text.Refresh()
 	})
 	content := container.NewVBox(inp, green, red, blue, button, text)
 	window.SetContent(content)
